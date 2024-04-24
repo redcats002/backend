601ab03443fb8201391541efed3b450f93b76966
redcats002@gitlab <redcats_002@hotmail.com>
2024-04-25 03:58:07 +0700

    👽️ ✨ CORS config

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const env = require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
+// const corsOptions = {
+//   origin: [],
+//   optionsSuccessStatus: 200,
+// };
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(require("./controller"));
